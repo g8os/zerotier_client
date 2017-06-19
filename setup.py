a5d1244ff9cb3a3a941131b5d52ce0601c9ce37b
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-06-19 14:00:03 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='zerotier',
+    version='1.1.0',
+    description='Zerotier API client',
+    long_description=long_description,
+    url='https://github.com/g8os/grid',
+    author='GIG',
+    author_email='info@gig.tech',
+    license='Apache 2.0',
+    packages=find_packages(),
+    install_requires=['requests'],
+)
