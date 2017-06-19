b366192208b2cb94ef1d5bc719fd0f5cafe1a4d6
Sarah Boctor <boctor.sarah@gmail.com>
2017-06-19 16:44:34 +0200

    Regenerate existing files.

--- zerotier/self_service.py
+++ zerotier/self_service.py
@@ -2,10 +2,12 @@ class SelfService:
     def __init__(self, client):
         self.client = client
 
-    def getAuthenticatedUser(self, headers=None, query_params=None):
+
+
+    def getAuthenticatedUser(self, headers=None, query_params=None, content_type="application/json"):
         """
         Get currently authenticated user (if any).
         It is method for GET /self
         """
         uri = self.client.base_url + "/self"
-        return self.client.session.get(uri, headers=headers, params=query_params)
+        return self.client.get(uri, None, headers, query_params, content_type)
